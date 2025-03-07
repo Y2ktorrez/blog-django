5f52fec6aa2c673b94ec090e0e59be78c1cd9456
Y2ktorrez <andrestorrezvaca@gmail.com>
2025-03-07 19:12:18 -0400

    pruebas

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,14 +1,39 @@
 from rest_framework import serializers
 from .models import Post, Category, Heading
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "name",
+            "slug",
+        ]
+
+class HeadingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Heading
+        fields = [
+            "title",
+            "slug",
+            "lavel",
+            "order",
+        ]
 
 class PostSerializer(serializers.ModelSerializer):
+    category = CategoryListSerializer()
+    headings = HeadingSerializer(many=True)
     class Meta:
         model = Post
         fields = "__all__"
 
 
 class PostListSerializer(serializers.ModelSerializer):
+    category = CategoryListSerializer()
     class Meta:
         model = Post
         fields = [
@@ -20,19 +45,3 @@ class PostListSerializer(serializers.ModelSerializer):
             "category",
         ]
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
-
-
-class HeadingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Heading
-        fields = [
-            "title",
-            "slug",
-            "level",
-            "order",
-        ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import ListAPIView
-from .models import Post
-from .serializers import PostListSerializer, PostSerializer
+from .models import Post, Heading
+from .serializers import PostListSerializer, PostSerializer, HeadingSerializer
 
 class PostListView(ListAPIView):
     queryset = Post.postobjects.all()
@@ -10,3 +10,10 @@ class PostDetailView(ListAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     lookup_field = 'slug'
+
+class PostHeadingsView(ListAPIView):
+    serializer_class = HeadingSerializer
+
+    def get_queryset(self):
+        post_slug = self.kwargs.get('slug')
+        return Heading.objects.filter(post__slug=post_slug)
